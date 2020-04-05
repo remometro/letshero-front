5221d151e7e64ab439c321e72cff0f779ede92c4
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2020-04-05 09:51:50 -0300

    Adding container to subtitles

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -2,17 +2,19 @@
   <main ref="map" class="map" v-if="isLoggedIn">
     <iframe @load="setHeight" ref="map__iframe" width="100%" height="auto" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=10&center=40.7128%2C-74.0060&key=AIzaSyDi_EQFS5kLRRj08KLH0w844aehqKNXOyw" allowfullscreen></iframe>
     <div class="map__subtitle">
-      <div class="map__subtitle__item">
-        <img src="@/assets/imgs/heart-green.svg" alt="" class="map__subtitle__item__img">
-        <span class="map__subtitle__item__caption text-green">Non <br/> urgent</span>
-      </div>
-      <div class="map__subtitle__item">
-        <img src="@/assets/imgs/heart-yellow.svg" alt="" class="map__subtitle__item__img">
-        <span class="map__subtitle__item__caption text-yellow">Medium <br/> urgent</span>
-      </div>
-      <div class="map__subtitle__item">
-        <img src="@/assets/imgs/heart-red.svg" alt="" class="map__subtitle__item__img">
-        <span class="map__subtitle__item__caption text-red">Very <br/> urgent</span>
+      <div class="lh-container">
+        <div class="map__subtitle__item">
+          <img src="@/assets/imgs/heart-green.svg" alt="" class="map__subtitle__item__img">
+          <span class="map__subtitle__item__caption text-green">Non <br/> urgent</span>
+        </div>
+        <div class="map__subtitle__item">
+          <img src="@/assets/imgs/heart-yellow.svg" alt="" class="map__subtitle__item__img">
+          <span class="map__subtitle__item__caption text-yellow">Medium <br/> urgent</span>
+        </div>
+        <div class="map__subtitle__item">
+          <img src="@/assets/imgs/heart-red.svg" alt="" class="map__subtitle__item__img">
+          <span class="map__subtitle__item__caption text-red">Very <br/> urgent</span>
+        </div>
       </div>
     </div>
   </main>
@@ -60,6 +62,11 @@ export default {
     align-items: center;
     justify-content: space-between;
     padding: 0 2rem;
+
+    .lh-container {
+      display: flex;
+      justify-content: space-between;
+    }
 
     &__item {
       display: flex;
