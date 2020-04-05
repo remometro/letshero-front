a901b6e920722effd0af06d2cc3d674f0f60bf24
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2020-04-05 09:22:57 -0300

    Another map height issue being fixed

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="map" v-if="isLoggedIn">
+  <main ref="map" class="map" v-if="isLoggedIn">
     <iframe @load="setHeight" ref="map__iframe" width="100%" height="auto" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=10&center=40.7128%2C-74.0060&key=AIzaSyDi_EQFS5kLRRj08KLH0w844aehqKNXOyw" allowfullscreen></iframe>
   </main>
   <main class="not-logged" v-else>
@@ -25,6 +25,7 @@ export default {
     setHeight() {
       let height = window.innerHeight
       this.$refs.map__iframe.height = height - 100
+      this.$refs.map.style.maxHeight = height - 200 + "px"
     }
   }
 }
@@ -33,7 +34,6 @@ export default {
 <style lang="scss">
 .map {
   width: 100vw;
-  max-height: calc(100vh - 200px);
   max-width: 100vw !important;
 }
 </style>
