1f18ca4da1ff0532b5f5702c9e8421df4b10d3e3
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2020-04-06 10:55:43 -0300

    Componentizing dropdowns

--- src/views/FindAHero.vue
+++ src/views/FindAHero.vue
@@ -1,14 +1,9 @@
 <template>
   <main class="find-a-hero">
     <form action="" class="find-a-hero__form">
-      <div class="lh--input--select find-a-hero__form__what">
-        <label for="find-a-hero__form__what__label">What do you need?</label>
-        <input type="text" required />
-      </div>
-      <div class="lh--input--select find-a-hero__form__do-reward">
-        <label for="find-a-hero__form__do-reward__label">Do you offer a reward?</label>
-        <input type="text" required />
-      </div>
+
+      <LH-Dropdown ref="typeOpen" label="What do you need?" :options="getTypesOfHelp" @selected="setType" :isOpen="typeOpen" />
+      <LH-Dropdown ref="typeOpen" label="Do you offer a reward?" :options="getOptionsHasReward" @selected="setType" :isOpen="typeOpen" />
       <div class="lh--input--text find-a-hero__form__reward">
         <label for="find-a-hero__form__reward__label">If so, how much? (in USD)</label>
         <input type="number" min="1" step="1" />
@@ -27,9 +22,75 @@
 </template>
 
 <script>
+import LHDropdown from "./components/LH-Dropdown"
 export default {
   name: "FindAHero",
-  components: {}
+  components: {
+    LHDropdown
+  },
+  data() {
+    return {
+      formTypeLabel: null,
+      formTypeValue: null,
+      typeOpen: false,
+      formHasRewardLabel: null,
+      formHasRewardValue: null,
+      hasRewardOpen: false
+    }
+  },
+  computed: {
+    getTypesOfHelp() {
+      return [
+        {
+          "label": "Food",
+          "value": "1"
+        },
+        {
+          "label": "Shelter",
+          "value": "2"
+        },
+        {
+          "label": "Medical Care",
+          "value": "3"
+        },
+        {
+          "label": "Companion",
+          "value": "4"
+        },
+        {
+          "label": "Other",
+          "value": "5"
+        }
+      ]
+    },
+    getOptionsHasReward() {
+      return [
+        {
+          "label": "I offer some cash",
+          "value": 1
+        },
+        {
+          "label": "I need some cash or help",
+          "value": 2
+        },
+        {
+          "label": "I need some cash only",
+          "value": 3
+        },
+        {
+          "label": "I need some help only",
+          "value": 4
+        }
+      ]
+    }
+  },
+  methods: {
+    setType(value) {
+      this.formTypeLabel = value[1]
+      this.formTypeValue = value[0]
+      this.toggleSelect('typeOpen')
+    }
+  }
 }
 </script>
 
---
+++ src/views/components/LH-Dropdown.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="lh-dropdown" v-on-clickaway="closeSelect">
+    <div class="lh--input--select find-a-hero__form__what" :class="{isOpen: isOpen}">
+      <div class="lh--input--select--field">
+        <label for="find-a-hero__form__what__label">{{label}}</label>
+        <input type="text" required v-model="selectValue" readonly @click="toggleSelect()"/>
+      </div>
+    </div>
+    <div class="lh-dropdown__options" :class="{isOpen: isOpen}" >
+      <div class="lh-dropdown__options__option" :key="option.value" v-for="option in options" @click="setValue(option.value, option.label)">
+        <span class="lh-dropdown__options__option__label">{{option.label}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mixin as clickaway } from 'vue-clickaway'
+export default {
+  mixins: [ clickaway ],
+  data() {
+    return {
+      selectValue: null,
+      isOpen: false
+    }
+  },
+  props: {
+    options: Array,
+    label: String
+  },
+  methods: {
+    setValue(value, label) {
+      this.toggleSelect()
+      this.selectValue = label
+      this.$emit("selected", value)
+    },
+    toggleSelect() {
+      this.isOpen = !this.isOpen
+    },
+    closeSelect() {
+      this.isOpen = false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.lh-dropdown {
+  position: relative;
+}
+.lh-dropdown__options {
+  display: none;
+  position: absolute;
+  background-color: $color-white;
+  width: 100%;
+  top: 49px;
+  z-index: 1;
+  color: $color-black;
+  max-height: 300px;
+  overflow-y: scroll;
+  cursor: pointer;
+
+  &__option {
+    padding: 1rem .5rem;
+    border: 1px solid $color-black;
+  }
+
+  &.isOpen {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+</style>
