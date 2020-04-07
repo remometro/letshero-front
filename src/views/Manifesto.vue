d0d42d2ec0e5a5c36e567b931e03acfe9737e849
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2020-04-06 21:13:44 -0300

    updating manifesto

--- src/views/Manifesto.vue
+++ src/views/Manifesto.vue
@@ -9,30 +9,34 @@
             Sketched by the dreams of many.
 <br />
 Dreamed by the sketches of one.
+
+<br /><br />
+Born in 2012 as KARUṆĀ, then krn.cc ™, Let's Hero is a story of freedom.
 <br /><br />
 Initially serving as a web-domain for KARUṆĀ, krn.cc ™ found autonomy over time & space while interceding with the dreams of a kid who always imagined life as an infinite playground.
 <br /><br />
-Born in 1992 as Angelo Reale, krn.cc ™ is a story of freedom.
-<br /><br />
-If freedom is to be here, now and conscious, I believe it'll never be found in roles but rather in work.
+If freedom is to be here, now and conscious, we believe it'll never be found in roles but rather in work.
 <br /><br />
 In a true analog system one can never fully account for mistakes without devising digitization methods for specifying needs in such an ordered manner that every interference is therefore automatically restored.
 <br /><br />
-In spite of sounding terrible from an humanistic perspective, what Angelo means is that by delegating the option for balancing the economy back to each individual transaction the over-rated speculative asset driven economy will gradually lose space to a more conscious, sustainable and efficient one.
+In spite of sounding terrible from an humanistic perspective, what we mean is that by delegating the option for balancing the economy back to each individual transaction, the over-rated speculative asset driven economy will gradually lose space to a more conscious, sustainable and efficient one.
 <br /><br />
-The means for achieving that lies more in the creation of elegant algorithms and data visualization tools that empower each user profile to be not only aware of hers or his current relationships and paths to choose, but also the impact they will have on the universal economy.
+The means for achieving that lies more in the creation of elegant algorithms and data visualization tools that empower each user profile to be, not only aware of hers or his current relationships and paths to choose, but also the impact they will have on the universal economy.
 Or approximation. Like branches that intercede.
+<br /><br />
 Yes, with a made-to-measure interface one can pretty much be aware of the consequences a particular decision partakes and towards which philosophical direction a particular node might be leading to.
 <br /><br />
 When it comes to volunteering, care-free engagement, NGO's, task commitment, crowd-funding, and mutual investment, this model helps each one of us clear up any doubts and questions that might be raised when opting for supporting a particular project, as it also empowers us to become true kings of not only our fate, but the fate of the whole existence.
 <br /><br />
-One very particular feature of gaming I find fascinating, is that sense of multi-dimensional immortality that manifests itself in many degrees depending on the frames, partnerships, challenges & settings you impose yourself with while experiencing it.
+One very particular feature of gaming we find fascinating, is that sense of multi-dimensional immortality that manifests itself in many degrees depending on the frames, partnerships, challenges & settings you impose yourself with while experiencing it.
 <br /><br />
-Now imagine a game, app or software, whatever call it, that provides not only such tools but one solution to the cascade effect current societies face when dealing with error and interference. Mostly marginalizing common human beings to antiquated reformatories or sentencing them to social isolation, detention or even just bullying, societies seem to have not yet understood that they are simply not succeeding & excelling when it comes to managing human resources and achieving true reintegration of a being.
+Now imagine a game, app or software, whatever you call it, that provides not only such tools, but one solution to the cascade effect current societies face when dealing with error and interference. Mostly marginalizing common human beings to antiquated reformatories or sentencing them to social isolation, detention or even just bullying, societies seem to have not just yet understood that they are simply not succeeding & excelling when it comes to managing human resources and achieving true reintegration of a being.
 <br /><br />
-Call it spare lives, game over, handicap, restart, unconditional love. One experience immortality in many levels when playing any virtual game. The mere acknowledgement that: somewhere, somehow, I can be somewhat shot and still keep on playing is both as daunting as appealing to me.
+Call it spare lives, game over, handicap, restart, unconditional love. One experience immortality in many levels when playing any virtual game. The mere acknowledgement that: somewhere, somehow, we can be somewhat shot and still keep on playing, is both as daunting as appealing to us.
 <br /><br />
-Now, talking about a serious way one can experience infinite forgiveness and learning is by always giving them the possibility to create a new beginning. If I know now I've invested on something that later on revealed to be corruptive and fraudulent, how can I give back to the balance of things what I've helped to take?
+Now, talking about a serious way one can experience infinite forgiveness and learning is by always giving them the possibility to create a new beginning.
+<br /><br />
+If we know now we've invested on something that later on revealed to be corruptive and fraudulent, how can we give back to the balance of things what we've helped to take?
 <br /><br />
 The very user experience of a video-game is at the same time: (a) absolutely engaging, (b) often effective & (c) mostly satisfactory.
 <br /><br />
