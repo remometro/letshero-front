4e0197b55f4d27328cc1ef327aa092090dc1ebe9
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2020-04-08 03:33:28 -0300

    Improving dropdown accessibility

--- src/views/components/LH-Dropdown.vue
+++ src/views/components/LH-Dropdown.vue
@@ -3,12 +3,14 @@
     <div class="lh--input--select find-a-hero__form__what" :class="{isOpen: isOpen}">
       <div class="lh--input--select--field">
         <label for="find-a-hero__form__what__label">{{label}}</label>
-        <input type="text" :disabled="disabled" required v-model="selectValue" readonly @click="!disabled && toggleSelect()" :placeholder="placeholder"/>
-      </div>
-    </div>
-    <div class="lh-dropdown__options" :class="{isOpen: isOpen}" >
-      <div class="lh-dropdown__options__option" :key="option.value" v-for="option in options" @click="setValue(option.value, option.label)">
-        <span class="lh-dropdown__options__option__label">{{option.label}}</span>
+        <button class="lh-dropdown__select-button" @click.stop="!disabled && toggleSelect($event)">
+          <input type="text" readonly tabindex="-1" :disabled="disabled" required v-model="selectValue" @click="!disabled && toggleSelect" @keypress.enter="!disabled && toggleSelect" :placeholder="placeholder"/>
+          <div class="lh-dropdown__options" :class="{isOpen: isOpen}" >
+            <button tabindex="0" class="lh-dropdown__options__option" :key="option.value" v-for="option in options" @click.stop="setValue($event, option.value, option.label)" >
+              <span class="lh-dropdown__options__option__label">{{option.label}}</span>
+            </button>
+          </div>
+        </button>
       </div>
     </div>
   </div>
@@ -31,12 +33,15 @@ export default {
     disabled: Boolean
   },
   methods: {
-    setValue(value, label) {
-      this.toggleSelect()
+    setValue($event, value, label) {
+      event.preventDefault()
+      this.toggleSelect($event)
       this.selectValue = label
       this.$emit("selected", value)
     },
-    toggleSelect() {
+    toggleSelect(e) {
+      e.preventDefault()
+      console.log("here")
       this.isOpen = !this.isOpen
     },
     closeSelect() {
@@ -52,13 +57,32 @@ export default {
   width: 100%;
   max-width: 300px;
   margin: 0 auto;
+
+  input {
+    border: none;
+    border-bottom: 2px solid $color-black;
+    font-family: $font-main;
+    color: $color-black;
+    width: 100%;
+    cursor: pointer;
+    background: none;
+    &:focus {
+      outline: none;
+    }
+  }
+
+  &__select-button {
+    border: none;
+    background: none;
+    padding: 0;
+  }
 }
 .lh-dropdown__options {
   display: none;
   position: absolute;
   background-color: $color-white;
   width: 99%;
-  top: 49px;
+  top: 33px;
   z-index: 1;
   color: $color-black;
   max-height: 300px;
@@ -69,6 +93,8 @@ export default {
   &__option {
     padding: 1rem .5rem;
     border: 1px solid $color-black;
+    font-family: $font-main;
+    color: $color-black;
   }
 
   &.isOpen {
