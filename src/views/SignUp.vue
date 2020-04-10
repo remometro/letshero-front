18a9704b2c279296274c71f9d8a2c1d33ca9c7bf
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2020-04-10 11:08:44 -0300

    sign up validation

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,44 +1,47 @@
 <template>
   <main class="signup">
-    <form action="" class="signup__form" @submit="signUp" v-if="!isLoggedIn">
-
-      <validation-provider class="lh--input--text signup__form__user" rules="required|alpha_dash|min:3|max:12" v-slot="{ errors }">
-        <label for="signup__form__user__input">Heroname</label>
-        <input type="text" v-model="formUser" name="heroname" data-vv-as="Hero Name" />
-        <span class="lh--error--message">{{ errors[0] }}</span>
-      </validation-provider>
-      <validation-observer class="lh--input--observer">
-        <validation-provider name="password" class="lh--input--text signup__form__pass" rules="required|min:6" v-slot="{ errors }">
-          <label for="signup__form__user__input">Password</label>
-          <input type="password" required v-model="formPass" />
+    <ValidationObserver v-slot="{ invalid }" v-if="!isLoggedIn">
+      <form action="" class="signup__form" @submit="signUp($event, invalid)">
+        <validation-provider class="lh--input--text signup__form__user" rules="required|alpha_dash|min:3|max:12" v-slot="{ errors }">
+          <label for="signup__form__user__input">Heroname</label>
+          <input type="text" v-model="formUser" name="heroname" data-vv-as="Hero Name" />
           <span class="lh--error--message">{{ errors[0] }}</span>
         </validation-provider>
-        <validation-provider name="confirmpass" class="lh--input--text signup__form__pass" rules="required|min:6|password:@password" v-slot="{ errors }">
-          <label for="signup__form__user__input">Confirm password</label>
-          <input type="password" required v-model="formPassVer" />
+        <validation-observer class="lh--input--observer">
+          <validation-provider name="password" class="lh--input--text signup__form__pass" rules="required|min:6" v-slot="{ errors }">
+            <label for="signup__form__user__input">Password</label>
+            <input type="password" v-model="formPass" />
+            <span class="lh--error--message">{{ errors[0] }}</span>
+          </validation-provider>
+          <validation-provider name="confirmation pass" class="lh--input--text signup__form__pass" rules="required|min:6|password:@password" v-slot="{ errors }">
+            <label for="signup__form__user__input">Confirm password</label>
+            <input type="password" v-model="formPassVer" />
+            <span class="lh--error--message">{{ errors[0] }}</span>
+          </validation-provider>
+        </validation-observer>
+        <validation-provider name="email" class="lh--input--text signup__form__email" rules="required|email" v-slot="{ errors }">
+          <label for="signup__form__user__email">Email</label>
+          <input type="email" v-model="formEmail" />
           <span class="lh--error--message">{{ errors[0] }}</span>
         </validation-provider>
-      </validation-observer>
-      <validation-provider name="email" class="lh--input--text signup__form__pass" rules="required|email" v-slot="{ errors }">
-        <label for="signup__form__user__email">Email</label>
-        <input type="email" required v-model="formEmail" />
-        <span class="lh--error--message">{{ errors[0] }}</span>
-      </validation-provider>
-      <div class="lh--input--text signup__form__paypal">
-        <label for="signup__form__user__paypal">Paypal email (to receive donations you must setup a PayPal merchant account with this email)</label>
-        <input type="email" required v-model="formPaypal" />
-      </div>
-      <LH-Dropdown rules="required" fieldName="Gender" ref="typeOpen" label="What do you identify with?" :options="getOptionsGender" @selected="setGender" />
-      <div class="lh--input--text signup__form__whatsapp">
-        <label for="signup__form__user__whatsapp">What's your whatsapp number?</label>
-        <input type="tel" pattern="+^[0-9-+\s()]*$" placeholder="+55123456789" v-mask="'+##############'" v-model="formWhatsapp" required>
-      </div>
-      <button class="lh--button signup__form__submit" @click="signUp">
-        {{ !isSigningUp ? "Sign Up" : "" }} <img class="lh--spinner-btn" src="../assets/imgs/spinner.svg" v-if="isSigningUp" />
-      </button>
-      <div class="lh--alert lh--alert--warning" v-if="signupError">{{signupErrorMessage}}</div>
-      <router-link to="/login" class="lh--link--black signup__form__existing">Existing hero?</router-link>
-    </form>
+        <validation-provider name="paypal email" class="lh--input--text signup__form__paypal" rules="email" v-slot="{ errors }">
+          <label for="signup__form__user__paypal">Paypal email (to receive donations you must setup a PayPal merchant account with this email)</label>
+          <input type="email" v-model="formPaypal" />
+          <span class="lh--error--message">{{ errors[0] }}</span>
+        </validation-provider>
+        <LH-Dropdown rules="required" fieldName="Gender" ref="typeOpen" label="What do you identify with?" :options="getOptionsGender" @selected="setGender" />
+        <validation-provider name="whatsapp number" class="lh--input--text signup__form__whatsapp" rules="required" v-slot="{ errors }">
+          <label for="signup__form__user__whatsapp">What's your whatsapp number?</label>
+          <input type="tel" placeholder="+55123456789" v-mask="'+##############'" v-model="formWhatsapp" />
+          <span class="lh--error--message">{{ errors[0] }}</span>
+        </validation-provider>
+        <button class="lh--button signup__form__submit" @click="signUp($event, invalid)">
+          {{ !isSigningUp ? "Sign Up" : "" }} <img class="lh--spinner-btn" src="../assets/imgs/spinner.svg" v-if="isSigningUp" />
+        </button>
+        <div class="lh--alert lh--alert--warning" v-if="signupError || hasErrors"><span class="error-message" v-if="signupError && !hasErrors">{{signupErrorMessage}}</span><span class="error-message" v-if="hasErrors && !signupError">{{generalError}}</span></div>
+        <router-link to="/login" class="lh--link--black signup__form__existing">Existing hero?</router-link>
+      </form>
+    </ValidationObserver>
     <div class="signup__islogged" v-else>
       <h1 class="signup__islogged__welcome">
         Welcome hero!
@@ -67,7 +70,9 @@ export default {
       formPass: null,
       formPassVer: null,
       formPaypal: null,
-      formWhatsapp: null
+      formWhatsapp: null,
+      hasErrors: false,
+      generalError: ''
     }
   },
   computed: {
@@ -107,10 +112,17 @@ export default {
     setGender(value) {
       this.formGender = value
     },
-    signUp(e) {
+    signUp(e, invalid) {
       e.preventDefault()
-      let payload = { username: this.formUser, password: this.formPass, password_verify: this.formPassVer, email: this.formEmail, whatsapp: this.formWhatsapp, paypal: this.formPaypal, gender: this.formGender }
-      this.$store.dispatch('signUp', payload)
+      if (!invalid) {
+        this.hasErrors = false
+        let payload = { username: this.formUser.toLowerCase(), password: this.formPass, password_verify: this.formPassVer, email: this.formEmail, whatsapp: this.formWhatsapp, paypal: this.formPaypal, gender: this.formGender }
+        this.$store.dispatch('signUp', payload)
+      } else {
+        this.hasErrors = true
+        this.generalError = "Form has errors! Please fix them and try again."
+        console.log("form with errors!")
+      }
     }
   }
 }
