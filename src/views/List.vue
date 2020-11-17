27e5be2613c9750eeea334b6936151c1058abb48
Angelo Reale <angeloreale@pm.me>
2020-11-17 00:02:01 -0300

    fix filter completed bug?

--- src/views/List.vue
+++ src/views/List.vue
@@ -51,7 +51,7 @@ export default {
       return this.$store.state.userData
     },
     entries() {
-      return this.$store.state.allHelpsData.concat(this.$store.state.paginatedAllHelpsData)
+      return _.filter(this.$store.state.allHelpsData.concat(this.$store.state.paginatedAllHelpsData), (help) => !help.stats.completed)
     },
     isLoggedIn() {
       return !!this.$store.state.isLoggedIn
